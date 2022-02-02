f37a9831228a1db8ba04200a5684ba650d9143a2
Aurnab Das <aurnabdas@Aurnabs-MacBook-Pro.local>
2022-02-02 02:14:10 -0500

    added smaller.cpp

---
+++ smaller.cpp
@@ -0,0 +1,11 @@
+/*
+Author: Aurnab Das
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab1A
+
+Two Values are inputed and program determines the value that is smaller
+*/
+
+#include<iostream>
+
