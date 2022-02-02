3f6cc8971d40cce083754e66c5447a6a71ceae77
Aurnab Das <aurnabdas@Aurnabs-MacBook-Pro.local>
2022-02-02 13:20:42 -0500

    finished first assignment

--- smaller.cpp
+++ smaller.cpp
@@ -9,3 +9,20 @@ Two Values are inputed and program determines the value that is smaller
 
 #include<iostream>
 
+int main()
+{
+int x,y;
+std::cout << "Type a number:";
+std::cin >> x;
+std::cout << "Type a number:";
+std::cin >> y;
+
+if (x>y){
+    std::cout << y << " is the smaller value.\n";
+} else {
+    std::cout << x << " is the smaller value.\n";
+   
+}
+return 0;
+
+}
