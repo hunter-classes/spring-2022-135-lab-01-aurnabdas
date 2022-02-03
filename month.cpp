5cf2114d3a351dfc739072e180828682c406d287
Aurnab Das <aurnabdas@Aurnabs-MacBook-Pro.local>
2022-02-03 08:35:21 -0500

    corrected error in month.cpp

--- month.cpp
+++ month.cpp
@@ -21,9 +21,9 @@ std::cin >> month;
 
 if (year % 4 == 0){
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 ||month == 12){
-        std::cout << "29 days\n";
-    } else if (month == 2){
         std::cout << "31 days\n";
+    } else if (month == 2){
+        std::cout << "29 days\n";
     } else{
         std::cout << "30 days\n";
     }
